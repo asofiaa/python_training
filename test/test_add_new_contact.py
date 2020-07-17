ee3b1c2db028e4fd7e13103608391d71f1ab4d2d
s.alekseeva <s.alekseeva@medpoint24.ru>
2020-07-17 17:23:24 +0300

    Dedicated class for working with groups and contacts

--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
 from fixture.application import Application
-from contact import Contact
+from model.contact import Contact
 
 
 @pytest.fixture()
@@ -14,6 +14,6 @@ def app(request):
 def test_add_new_contact(app):
     app.open_home_page()
     app.session.login(username="admin", password="secret")
-    app.create_new_contact(Contact(firstname=u"Sofia", middlename="Sofia", lastname="Alekseeva", nickname="asofiaa", title="title", company="company", address="address", home="123", mobile="123", work="123", fax="123", email="aaa@aaa.ru", email2="aaa@aaa.ru", email3="aaa@aaa.ru", homepage="google.com", bday="15", bmonth="December", byear="1995", aday="10", amonth="January", ayear="1996", address2="address2", phone2="123", notes="notes" ))
-    app.return_to_the_home_page()
+    app.contact.create(Contact(firstname=u"Sofia", middlename="Sofia", lastname="Alekseeva", nickname="asofiaa", title="title", company="company", address="address", home="123", mobile="123", work="123", fax="123", email="aaa@aaa.ru", email2="aaa@aaa.ru", email3="aaa@aaa.ru", homepage="google.com", bday="15", bmonth="December", byear="1995", aday="10", amonth="January", ayear="1996", address2="address2", phone2="123", notes="notes"))
+    app.contact.return_to_the_home_page()
     app.session.logout()
--- test/test_add_new_group.py
+++ test/test_add_new_group.py
@@ -13,11 +13,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name=u"Новая группа", header=u"хедер", footer=u"футер"))
+    app.group.create(Group(name=u"Новая группа", header=u"хедер", footer=u"футер"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name=u"", header=u"", footer=u""))
+    app.group.create(Group(name=u"", header=u"", footer=u""))
     app.session.logout()
